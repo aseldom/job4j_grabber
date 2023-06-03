f9ba69d11c89a264e55462796aafed890bf1bba4
aseldom <a.seldom@gmail.com>
2023-06-03 16:00:33 +0600

    7. Web. [#289478]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -78,6 +78,7 @@ public class Grabber implements Grab {
     }
 
     public static void main(String[] args) throws Exception {
+
         var cfg = new Properties();
         try (InputStream in = Grabber.class.getClassLoader()
                 .getResourceAsStream("app.properties")) {
